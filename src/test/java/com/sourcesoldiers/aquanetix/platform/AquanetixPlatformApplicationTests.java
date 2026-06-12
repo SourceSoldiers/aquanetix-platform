3b5f0dbf1d617453fa5edf529e9c8708368ec8a9
DeterminedSoul7 <u202411402@upc.edu.pe>
2026-06-11 20:00:22 -0500

    chore: initial commmit

---
+++ src/test/java/com/sourcesoldiers/aquanetix/platform/AquanetixPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sourcesoldiers.aquanetix.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquanetixPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
