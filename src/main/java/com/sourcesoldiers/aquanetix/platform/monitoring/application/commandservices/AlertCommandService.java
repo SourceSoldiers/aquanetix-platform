33a588ca212bbb0272fcc7fe16a6f06d72331641
NicoCSE <u20241D428@upc.edu.pe>
2026-06-20 16:47:19 -0500

    feat(alert): add handle method for ResolveAlertCommand in AlertCommandService

--- src/main/java/com/sourcesoldiers/aquanetix/platform/monitoring/application/commandservices/AlertCommandService.java
+++ src/main/java/com/sourcesoldiers/aquanetix/platform/monitoring/application/commandservices/AlertCommandService.java
@@ -2,9 +2,11 @@ package com.sourcesoldiers.aquanetix.platform.monitoring.application.commandserv
 
 import com.sourcesoldiers.aquanetix.platform.monitoring.domain.model.aggregates.Alert;
 import com.sourcesoldiers.aquanetix.platform.monitoring.domain.model.commands.CreateAlertCommand;
+import com.sourcesoldiers.aquanetix.platform.monitoring.domain.model.commands.ResolveAlertCommand;
 
 import java.util.Optional;
 
 public interface AlertCommandService {
     Optional<Alert> handle(CreateAlertCommand command);
+    Optional<Alert> handle(ResolveAlertCommand command); // Añadir esta línea
 }
