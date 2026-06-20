ad8dc88d6d86faafebeb89f9fa6b49c6602a927b
NicoCSE <u20241D428@upc.edu.pe>
2026-06-20 11:53:02 -0500

    feat(alert): add method to handle GetAllAlertsQuery in AlertQueryService

--- src/main/java/com/sourcesoldiers/aquanetix/platform/monitoring/application/queryservices/AlertQueryService.java
+++ src/main/java/com/sourcesoldiers/aquanetix/platform/monitoring/application/queryservices/AlertQueryService.java
@@ -2,8 +2,12 @@ package com.sourcesoldiers.aquanetix.platform.monitoring.application.queryservic
 
 import com.sourcesoldiers.aquanetix.platform.monitoring.domain.model.aggregates.Alert;
 import com.sourcesoldiers.aquanetix.platform.monitoring.domain.model.queries.GetAlertByIdQuery;
+import com.sourcesoldiers.aquanetix.platform.monitoring.domain.model.queries.GetAllAlertsQuery;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface AlertQueryService {
     Optional<Alert> handle(GetAlertByIdQuery query);
+    List<Alert> handle(GetAllAlertsQuery query);
 }
