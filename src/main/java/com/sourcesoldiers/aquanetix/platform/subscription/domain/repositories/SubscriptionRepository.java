62f4f10358df6be364cc9ee8f20fb800c66219ec
Sebastian Cochachi <sebastiancochachi02@gmail.com>
2026-06-20 15:05:09 -0500

    feat (subscription): create files

---
+++ src/main/java/com/sourcesoldiers/aquanetix/platform/subscription/domain/repositories/SubscriptionRepository.java
@@ -0,0 +1,7 @@
+package com.sourcesoldiers.aquanetix.platform.subscription.domain.repositories;
+
+import com.sourcesoldiers.aquanetix.platform.subscription.domain.model.aggregates.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+}
