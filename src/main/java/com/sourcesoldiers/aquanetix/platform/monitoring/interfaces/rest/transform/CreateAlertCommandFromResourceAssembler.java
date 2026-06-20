fc219ddadb83b41be3c56d6718a628a85c8f3b29
NicoCSE <u20241D428@upc.edu.pe>
2026-06-20 14:10:59 -0500

    feat(alert): add CreateAlertCommandFromResourceAssembler for converting CreateAlertResource to CreateAlertCommand

---
+++ src/main/java/com/sourcesoldiers/aquanetix/platform/monitoring/interfaces/rest/transform/CreateAlertCommandFromResourceAssembler.java
@@ -0,0 +1,19 @@
+package com.sourcesoldiers.aquanetix.platform.monitoring.interfaces.rest.transform;
+
+import com.sourcesoldiers.aquanetix.platform.monitoring.domain.model.commands.CreateAlertCommand;
+import com.sourcesoldiers.aquanetix.platform.monitoring.interfaces.rest.resources.CreateAlertResource;
+
+public class CreateAlertCommandFromResourceAssembler {
+    public static CreateAlertCommand toCommandFromResource(CreateAlertResource resource) {
+        return new CreateAlertCommand(
+                resource.deviceId(),
+                resource.deviceName(),
+                resource.location(),
+                resource.type(),
+                resource.severity(),
+                resource.message(),
+                resource.value(),
+                resource.threshold()
+        );
+    }
+}
