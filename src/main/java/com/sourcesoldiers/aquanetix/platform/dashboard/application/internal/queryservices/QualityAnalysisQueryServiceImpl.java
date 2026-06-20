dce3505b23d1ae4a10fd5c3cd138a6a9ea1eda87
Sebastian <u202414345@upc.edu.pe>
2026-06-20 01:12:23 -0500

    feat(quality-analysis): implement quality analysis retrieval by ID and related value objects

--- src/main/java/com/sourcesoldiers/aquanetix/platform/dashboard/application/internal/queryservices/QualityAnalysisQueryServiceImpl.java
+++ src/main/java/com/sourcesoldiers/aquanetix/platform/dashboard/application/internal/queryservices/QualityAnalysisQueryServiceImpl.java
@@ -2,11 +2,13 @@ package com.sourcesoldiers.aquanetix.platform.dashboard.application.internal.que
 
 import com.sourcesoldiers.aquanetix.platform.dashboard.application.queryservices.QualityAnalysisQueryService;
 import com.sourcesoldiers.aquanetix.platform.dashboard.domain.model.aggregates.QualityAnalysis;
+import com.sourcesoldiers.aquanetix.platform.dashboard.domain.model.queries.GetAllQualityAnalysesQuery;
 import com.sourcesoldiers.aquanetix.platform.dashboard.domain.model.queries.GetQualityAnalysisByIdQuery;
 import com.sourcesoldiers.aquanetix.platform.dashboard.domain.repositories.QualityAnalysisRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -27,5 +29,10 @@ public class QualityAnalysisQueryServiceImpl implements QualityAnalysisQueryServ
     @Override
     public Optional<QualityAnalysis> handle(GetQualityAnalysisByIdQuery query) {
         return qualityAnalysisRepository.findById(query.analysisId());
+    }
+
+    @Override
+    public List<QualityAnalysis> handle(GetAllQualityAnalysesQuery query) {
+        return qualityAnalysisRepository.findAll();
     }
 }
