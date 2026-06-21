70ab4fe7768df8e44b39fd41b4b9b1323245c1fb
Sebastian Cochachi <sebastiancochachi02@gmail.com>
2026-06-20 20:26:03 -0500

    fix (subscription): fix add create-subscription

---
+++ src/main/java/com/sourcesoldiers/aquanetix/platform/subscription/domain/model/commands/CreateSubscriptionCommand.java
@@ -0,0 +1,8 @@
+﻿package com.sourcesoldiers.aquanetix.platform.subscription.domain.model.commands;
+
+public record CreateSubscriptionCommand(
+        Integer userId,
+        String plan,
+        String status
+) {
+}
