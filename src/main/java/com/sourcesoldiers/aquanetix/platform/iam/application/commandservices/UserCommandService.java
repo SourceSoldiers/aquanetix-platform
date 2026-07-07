fb9576ea711e08ff4540006a4845c81fd8e2bc9d
DeterminedSoul7 <u202411402@upc.edu.pe>
2026-07-07 17:20:33 -0500

    feat(iam): add user and role command services

---
+++ src/main/java/com/sourcesoldiers/aquanetix/platform/iam/application/commandservices/UserCommandService.java
@@ -0,0 +1,9 @@
+package com.sourcesoldiers.aquanetix.platform.iam.application.commandservices;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+
+/**
+ * Application service contract for IAM user commands.
+ */
+public interface UserCommandService {
+}
